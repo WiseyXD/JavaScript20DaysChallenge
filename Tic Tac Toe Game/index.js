cd13e7e2c2e461ffefa25cd780c65aea8156bba9
Aryan Nagbanshi <aryan.s.nag@gmail.com>
2023-04-08 10:42:19 +0530

    Tic Tac Toe Game Completed

--- Tic Tac Toe Game/index.js
+++ Tic Tac Toe Game/index.js
@@ -3,6 +3,9 @@ const CIRCLE_CLASS = "circle";
 const cells = document.querySelectorAll(".data-cell");
 const board = document.getElementById("board");
 let circleTurn;
+const winningText = document.getElementById("winningText");
+const winningPage = document.querySelector(".winning-message");
+const resetButton = document.querySelector("#resetButton");
 const Winning_Combinations = [[0,1,2],
 [3,4,5],
 [6,7,8],
@@ -11,15 +14,45 @@ const Winning_Combinations = [[0,1,2],
 [2,5,8],
 [0,4,8],
 [2,4,6]];
-startGame();
 
+startGame();
+resetButton.addEventListener("click", startGame)
 function startGame ()
 {  
-    circleTurn =false;
+    circleTurn = false;
     cells.forEach((i)=> {
-        i.addEventListener("click",handleClick , {once: true})
+        i.classList.remove(CIRCLE_CLASS);
+        i.classList.remove(X_CLASS);
+        i.removeEventListener("click",handleClick , {once: true});
+        i.addEventListener("click",handleClick , {once: true});
     });
-    setBoardHoverClass(); 
+    // setBoardHoverClass(); 
+    winningPage.classList.remove("show");
+}
+
+function endGame(draw)
+{
+    if (draw === true)
+    {
+        winningText.innerText = "Draw!";
+    }
+    else
+    {
+        winningText.innerText = `${circleTurn ? "O Wins!" : "X Wins!"}`;
+        console.log("winner");
+    }
+    winningPage.classList.add("show");
+    
+}
+
+function checkDraw()
+{
+    if ([...cells].every(cell => {
+        return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+      }))
+      {
+          return true;
+      }
 }
 
 function handleClick(e)
@@ -32,12 +65,21 @@ function handleClick(e)
     //Check for win
     if (checkWin(currentClass))
     {
-        console.log("Winner");
+        endGame(false);
+    }
+    else if (checkDraw())
+    {
+        endGame(true);
     }
     //Check For draw
-    switchTurns();
-    setBoardHoverClass();
+    else
+    {
+        switchTurns();
+        // setBoardHoverClass();
+    } 
+    
 }
+
 
 function placeMarker(cell,currentClass)
 {
@@ -50,23 +92,22 @@ function switchTurns()
     circleTurn =!circleTurn;
 }
 
-function setBoardHoverClass() {
-    board.classList.remove(X_CLASS);
-    board.classList.remove(CIRCLE_CLASS);
-    if (circleTurn) {
-      board.classList.add(CIRCLE_CLASS);
-    } else {
-      board.classList.add(X_CLASS);
-    }
-  }
+// function setBoardHoverClass() {
+//     board.classList.remove(X_CLASS);
+//     board.classList.remove(CIRCLE_CLASS);
+//     if (circleTurn) {
+//       board.classList.add(CIRCLE_CLASS);
+//     } else {
+//       board.classList.add(X_CLASS);
+//     }
+//   }
 
 function checkWin(currentClass) 
 {
-    return Winning_Combinations.some((combination)=>
-    {
-        return combination.every((Index)=>
-        {
-            return cellElements[Index].classList.contains(currentClass);
-        });
+    return Winning_Combinations.some(combination => {
+      return combination.every(index => {
+        return cells[index].classList.contains(currentClass);
+      });
     });
-}
+  }
+
