6b704fb68f3d9531c14ca24088069fe6ad82626d
Aryan Nagbanshi <aryan.s.nag@gmail.com>
2023-05-13 02:34:32 +0530

    Callback done learnt how to wait for http ntwrk request to complete before executing the next line of code

--- 09-asynchronous-javascript/03-callbacks/script.js
+++ 09-asynchronous-javascript/03-callbacks/script.js
@@ -1,6 +1,37 @@
-function toggle(e) {
-  console.log('callback ran');
-  e.target.classList.toggle('danger');
+// function toggle(e) {
+//   console.log('callback ran');
+//   e.target.classList.toggle('danger');
+// }
+
+// document.querySelector('button').addEventListener('click', toggle);
+
+const posts = [
+  { title: "Aryan" , body: "OP" },
+  { title : "Nikshe" , body : "Nakli"}
+];
+
+function getPost() //Executes instantly
+{
+  posts.forEach(emt => {
+    const div = document.createElement ("div");
+    div.innerHTML = `<strong>${emt.title}</strong> - ${emt.body}`;
+    document.querySelector("#posts").appendChild(div);
+  });
 }
 
-document.querySelector('button').addEventListener('click', toggle);
+
+function addPost (titless , body1 , cb) //Executes after 2 seconds
+{
+  const post1 = {title : titless , body :body1};
+  posts.push(post1);
+  cb();
+} 
+
+function addPostCallback ()  //Executes after 2 seconds
+{
+  addPost("Sakshi" , "nakiliiiiii", getPost);
+  addPost("Vijaylaxmi" , "Moti", getPost);
+  console.log(posts);
+}
+
+
