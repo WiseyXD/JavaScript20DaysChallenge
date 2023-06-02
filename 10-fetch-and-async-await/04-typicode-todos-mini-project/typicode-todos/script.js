faf53dc572e14321f1860c10dc2b6bda012f99df
Aryan Nagbanshi <aryan.s.nag@gmail.com>
2023-06-02 13:15:31 +0530

    Typicode Project completed

--- 10-fetch-and-async-await/04-typicode-todos-mini-project/typicode-todos/script.js
+++ 10-fetch-and-async-await/04-typicode-todos-mini-project/typicode-todos/script.js
@@ -47,12 +47,54 @@ function createTodo(e) {
 }
 
 function toggleCompleted(e) {
-	console.log("1");
 	if (e.target.classList.contains("todoItems")) {
 		e.target.classList.toggle("done");
 	}
+
+	updateTodo(e.target.classList.contains("done"), e.target.dataset.id);
+}
+
+function updateTodo(completed, id) {
+	fetch(`${"https://jsonplaceholder.typicode.com/todos"}/${id}`, {
+		method: "PUT",
+		body: JSON.stringify({ completed }),
+		headers: {
+			"Content-Type": "application.json",
+		},
+	})
+		.then((res) => {
+			return res.json();
+		})
+		.then((data) => {
+			console.log(data);
+		});
+}
+
+function toDelete(e) {
+	console.log(e.target);
+	const removeTarget = e.target;
+	removeTarget.remove();
+
+	deletedTodo(e.target.dataset.id);
+}
+
+function deletedTodo(id) {
+	fetch(`${"https://jsonplaceholder.typicode.com/todos"}/${id}`, {
+		method: "DELETE",
+		// body: "",
+		// headers: {
+		// 	"Content-Type": "application/json",
+		// },
+	})
+		.then((res) => {
+			return res.json();
+		})
+		.then((data) => {
+			console.log(data);
+		});
 }
 
 document.addEventListener("DOMContentLoaded", getPosts);
 document.querySelector("#todo-form").addEventListener("submit", createTodo);
 document.querySelector("#todo-list").addEventListener("click", toggleCompleted);
+document.querySelector("#todo-list").addEventListener("dblclick", toDelete);
